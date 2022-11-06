e672d3bd3cd10b979edae55d4b70960cc06577cb
256ArtsDeveloper <256arts@jaydenirwin.com>
2022-11-06 17:38:12 -0500

    Initial Commit

---
+++ GC Browser/GC_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GC_BrowserApp.swift
+//  GC Browser
+//
+//  Created by Jayden Irwin on 2022-11-06.
+//
+
+import SwiftUI
+
+@main
+struct GC_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
